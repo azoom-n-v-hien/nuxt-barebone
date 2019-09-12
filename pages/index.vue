1765b119e23fa5fa951ed2e8efd92713e6127eff
azoom-n-v-hien <n.v.hien@azoom.jp>
2019-09-12 03:52:44 +0000

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <content-card>
-      <div slot="title" class="title-card">
+      <div slot="title" class="title">
         <img class="icon" src="@/assets/svg/warnning.svg" />
         <h4 class="text">
           2週間以上ステータスを変更していない案件
